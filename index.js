c185c519c14fc8497cd3a855c127022034fde649
Scott Lingner <slingner@gmail.com>
2019-12-05 12:39:48 -0800

    added js html and css

---
+++ index.js
@@ -0,0 +1,6 @@
+// const parent = document.getElementsByClassName('parent')[0];
+const parent = $('.parent')
+​
+$(parent).html('<h2> Starting with jQuery </h2>')
+​
+// parent.innerHTML = `<h2>Start Quiz</h2>`
