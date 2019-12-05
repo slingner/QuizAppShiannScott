e145e86fc4550515aed784273213b05a6f5fb9df
Scott Lingner <slingner@gmail.com>
2019-12-05 12:44:52 -0800

    fix links for js and css

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // const parent = document.getElementsByClassName('parent')[0];
-const parent = $('.parent')
-​
-$(parent).html('<h2> Starting with jQuery </h2>')
-​
+'use strict';
+
+const parent = $('.parent');
+$(parent).html('<h2> Starting with jQuery </h2>');
+
 // parent.innerHTML = `<h2>Start Quiz</h2>`
