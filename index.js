c8dc0f6dd59f08ea6dea6a998d993832a27ec78e
Scott Lingner <slingner@gmail.com>
2019-12-06 07:51:08 -0800

    lots of changes to js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // const parent = document.getElementsByClassName('parent')[0];
 'use strict';
 
-
-const store = {
+const STORE = {
   questions: [
     {
       question: 'What is the hottest Pepper in the world?',
@@ -23,16 +22,6 @@ const store = {
         'Destroying Angel'
       ],
       correctAnswer: 'Death Cap'
-    },
-    {
-      question: 'As early as 250 A.D., ancient civilizations of Mexico and South America, specifically The Mayans and the Aztecs, used this as a form of currency?',
-      answers: [
-        'Silver',
-        'Dollars',
-        'Wheat',
-        'Cocoa beans',
-      ],
-      correctAnswer: 'Cocoa beans'
     },
     {
       question: 'What did the Mayans and the Aztecs use as a form of currency?',
@@ -69,6 +58,136 @@ const store = {
   score: 0
 };
 
+
+function renderHomePage() {
+  const html = `
+    <h1>Extreme Food Trivia</h1>
+    <h2>Take a break from programming and dive into the weird world of food!</h2>
+    <div>
+      <button id="start-quiz">Start Quiz</button>
+    </div>
+  `;
+
+  $('header').html(html);
+}
+
+
+function questionTemplate(question) {
+  return `
+    <h1>${question.question}</h1>
+    <fieldset>
+      <div class="questionList">
+        <input class='answer' type="radio" name='answer' value='0' required"/> 
+        <label for="y">${question.answers[0]}</label><br>
+      <input class='answer' type="radio" name='answer' value='0' required/>
+        <label for="y">${question.answers[1]}</label><br>
+          <input class='answer' type="radio" name='answer' value='0' required/>
+        <label for="y">${question.answers[2]}</label><br>
+          <input class='answer' type="radio" name='answer' value='0' required/>
+        <label for="y">${question.answers[3]}</label><br>
+      </div>
+    </fieldset>
+    <button id="submit">Submit</button>
+    <button id="next-question">Next Question</button>
+  `;
+}
+
+// function statusTemplate() {
+//   return `
+//   <div class='status-container'>
+//     <div class = 'question-number'>
+//       <h3>Question 1 of 5</h3>
+//     </div class = 'question-number'>
+//     <div class = 'score-number'>
+//       <h5>Score: 0</h5>
+//       <h5>Number Correct: 0</h5>
+//       <h5>Number Incorrect: 0</h5>
+//     </div class = 'score-number'>
+//   </div>
+//   `;
+// }
+
+// function questionStatus(questions) {
+//   const questionNumber = questions.questionNumber;
+//   $('.question-number').empty();
+//   $('.question-number').append(
+//     `<p class= 'question-status'>Question ${questionNumber + 1} out of 10</p>`
+//   );
+// }
+
+// function scoreStatus(questions) {
+//   const scoreNumber = questions.score;
+//   $('.score-number').empty();
+//   $('.score-number').append(
+//     `<p>Score ${totalScore} out of 10</p>`
+//   );
+//   console.log('scoreStatus ran');
+//   console.log(totalScore);
+// };
+
+// function answersTemplate(question) {
+//   return `
+//     <h1>${question.answers}</h1>
+//   `;
+// }
+
+function questionSelector(questions) {
+  const questionNumber = questions.questionNumber; //current question number
+  const questionsArray = questions.questions; // all available questions
+
+  const question = questionTemplate(questionsArray[questionNumber]); // retrieve current question
+
+  return question; // return current question
+}
+
+
+function renderQuestion() {
+  const questionString = questionSelector(STORE); //this makes the variable questionString 
+  $('main').html(questionString); //put the html from const questionString into the main of index.html
+  STORE.questionNumber += 1; //this iterates through the question numbers
+  //so every click will trigger the html of the next question and also set the new question number for the next click
+}
+
+//on the button id #start-quiz click, return renderQuestion() 
+//renderQuestion()
+function handleStartQuiz() {
+  $('#start-quiz').click(function() {
+    renderQuestion();
+
+    $('header').html('');
+  });
+}
+
+function handleNextQuestion() {
+  $(document).on('click', '#next-question', function() {
+    renderQuestion();
+  });
+}
+
+// function renderSubmit() {
+ 
+// }
+
+// function handleSubmit() {
+//   $(document).on('click', '#submit', function() {
+//     renderSubmit();
+//   });
+// }
+ 
+
+// function handleFinishQuiz
+
+// function renderScoreScreen
+
+
+function quizHandler() {
+  renderHomePage();
+  handleStartQuiz();
+  handleNextQuestion();
+}
+
+$(quizHandler);
+
 /**
  *
  * Your app should include a render() function, that regenerates
@@ -82,9 +201,4 @@ const store = {
  *
  */
 
-
-
-const parent = $('.parent');
-$(parent).html('<h2> Starting with jQuery </h2>');
-
-// parent.innerHTML = `<h2>Start Quiz</h2>`
+// Initialize all functions that run the application
